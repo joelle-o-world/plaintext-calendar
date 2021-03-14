4ae361cea3b5e3721acf6bf53f62b6b5e572d20d
Joel Plowright <joelyjoel@protonmail.com>
2021-03-14 01:00:59 +0000

    use description as location

--- js/convertToICS.js
+++ js/convertToICS.js
@@ -1,17 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.convertToICS = void 0;
 var ics_1 = require("ics");
 var ics_tools_1 = require("./ics-tools");
 var HoursMinutes_1 = require("./HoursMinutes");
+var config_1 = __importDefault(require("./config"));
 function convertToICS(events, oldFile) {
-    console.log("kjfvkdjnbv", oldFile);
     return new Promise(function (fulfil, reject) {
         var preparedEvents = events.map(function (event) { return ({
             title: event.message,
             description: event.description,
             start: HoursMinutes_1.dateArray(event.startDatetime),
             end: HoursMinutes_1.dateArray(event.endDatetime),
+            location: config_1.default.useDescriptionAsLocation ? event.description : undefined,
         }); });
         ics_1.createEvents(preparedEvents, function (err, icsString) {
             if (err)
@@ -19,7 +23,6 @@ function convertToICS(events, oldFile) {
             else {
                 if (oldFile) {
                     var cancellations = ics_tools_1.cancelActiveEvents(oldFile);
-                    console.log('## cancellations:', cancellations);
                     icsString = ics_tools_1.insertEvents(cancellations, icsString);
                 }
                 fulfil(icsString);
--- src/convertToICS.ts
+++ src/convertToICS.ts
@@ -2,15 +2,16 @@ import {Event} from './parseCalendarText';
 import {createEvents} from 'ics'
 import {insertEvents, cancelActiveEvents} from './ics-tools'
 import {dateArray} from './HoursMinutes';
+import config from './config';
 
 export function convertToICS(events: Event[], oldFile?: string):Promise<string> {
-  console.log("kjfvkdjnbv", oldFile);
   return new Promise((fulfil, reject) => {
     let preparedEvents = events.map(event => ({
       title: event.message,
       description: event.description,
       start: dateArray(event.startDatetime),
       end: dateArray(event.endDatetime),
+      location: config.useDescriptionAsLocation ?  event.description : undefined,
     }))
 
     createEvents(preparedEvents, (err, icsString) => {
@@ -19,7 +20,6 @@ export function convertToICS(events: Event[], oldFile?: string):Promise<string> 
       else {
         if(oldFile) {
           let cancellations = cancelActiveEvents(oldFile);
-          console.log('## cancellations:', cancellations);
           icsString =  insertEvents(cancellations, icsString);
         }
         fulfil(icsString);
