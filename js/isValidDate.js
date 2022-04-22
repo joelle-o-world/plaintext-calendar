08d224fed019586e72524bb5d487f100aa5dde61
Joelle Plow <joelleplow@pm.me>
2022-04-22 20:22:28 +0100

    Gitignore built code

--- js/isValidDate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidDate = void 0;
-function isValidDate(d) {
-  return d instanceof Date && !isNaN(d.getTime());
-}
-exports.isValidDate = isValidDate;
-exports.default = isValidDate;
